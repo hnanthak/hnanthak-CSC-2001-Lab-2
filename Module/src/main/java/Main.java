8d52d05cd73e8982ee27a32534dfa94d88528ee2
harshitha <harshithananthakumar@gmail.com>
2026-09-08 16:19:36 -0700

    3.1 finished and tested

--- Module/src/main/java/Main.java
+++ Module/src/main/java/Main.java
@@ -3,94 +3,186 @@
 
 
 public class Main {
-    // add six to a number
 
-    public record date(int date, int month, int year){}
-//returns amount of dates when the number of a moth is specified
-    public static int daysInMonth(int month){
-        switch(month){
-            case 1:
-                System.out.println("31");
-                return(31);
-            case 2:
-                System.out.println("28");
-                return(28);
-            case 3:
-                System.out.println("31");
-                return(31);
-            case 4:
-                System.out.println("30");
-                return(30);
-            case 5:
-                System.out.println("31");
-                return(31);
-            case 6:
-                System.out.println("30");
-                return(30);
-            case 7:
-                System.out.println("31");
-                return(31);
-            case 8:
-                System.out.println("31");
-                return(31);
-            case 9:
-                System.out.println("30");
-                return(30);
-            case 10:
-                System.out.println("31");
-                return(31);
-            case 11:
-                System.out.println("30");
-                return(30);
-            case 12:
-                System.out.println("31");
-                return(31);
+    //creates a record of a date
+    public record date(int date, int month, int year) {
+        public date{
+            if (date>31 || date<1)
+            {
+                throw new IllegalArgumentException("invalid day");
+            }
+            if (month>12|| month<1){
+                throw new IllegalArgumentException("Incalid month");
+            }
         }
+    }
+
+    date date1 = new date(22, 1, 2008);
+    date date2 = new date(2, 12, 2001);
+    date date3 = new date(19, 12, 21);
+
+    //returns amount of dates when the number of a moth is specified
+    public static int daysInMonth(int month) {
+        int days = 0;
+        switch (month) {
+            case 1:
+                days = 31;
+                break;
+            case 2:
+                days = 28;
+                break;
+            case 3:
+                days = 31;
+                break;
+            case 4:
+                days = 30;
+                break;
+            case 5:
+                days = 31;
+                break;
+            case 6:
+                days = 30;
+                break;
+            case 7:
+                days = 31;
+                break;
+            case 8:
+                days = 31;
+                break;
+            case 9:
+                days = 30;
+                break;
+            case 10:
+                days = 31;
+                break;
+            case 11:
+                days = 30;
+                break;
+            case 12:
+                days = 31;
+                break;
+        }
+        System.out.println(days);
+        return days;
     }
 
 // returns the day after the date specified
 
-    public static String tommorow (int month,int day, int year){
-        int new_month=month;
-        int new_day=day;
-        int new_year=year+1;
-        if (day==31){
-            switch (month){
-                case 1:
-                    new_day=1;
-                    new_month=2;
-                case 3:
-                    new_day=1;
-                    new_month=4;
-                case 5:
-                    new_day=1;
-                    new_month=6;
-                case 7:
-                    new_day=1;
-                    new_month=8;
+    public static date tommorow(int day, int month, int year) {
+        int new_day = day;
+        int new_month = month;
+        int new_year = year;
+        if (month > 12 || day > 31) {
+            throw new IllegalArgumentException("Invalid month");
+        }
+        if (month == 2 && day == 28) {
+            new_day = 1;
+            new_month = month + 1;
+        } else if (day == 31) {
+            switch (month) {
+                case 4:
+                    new_day = 1;
+                    new_month = month + 1;
+                    break;
+                case 6:
+                    new_day = 1;
+                    new_month = month + 1;
+                    break;
+                case 9:
+                    new_day = 1;
+                    new_month = month + 1;
+                    break;
+                case 11:
+                    new_day = 1;
+                    new_month = month + 1;
+                    break;
+                case 8:
+                    new_day = 1;
+                    new_month = month + 1;
+                    break;
                 case 10:
-                    new_day=1;
-                    new_month=11;
+                    new_day = 1;
+                    new_month = month + 1;
+                    break;
                 case 12:
-                    new_day=1;
-                    new_month=1;
+                    new_day = 1;
+                    new_month = 1;
+                    new_year = year + 1;
+                    break;
             }
+        } else if (day == 30) {
+            switch (month) {
+                case 4:
+                    new_day = 1;
+                    new_month = month + 1;
+                    break;
+                case 6:
+                    new_day = 1;
+                    new_month = month + 1;
+                    break;
+                case 9:
+                    new_day = 1;
+                    new_month = month + 1;
+                    break;
+                case 11:
+                    new_day = 1;
+                    new_month = month + 1;
+                    break;
+            }
+        } else {
+            new_day = day + 1;
 
         }
-        else{
-            new_day=day+1;
-            new_month=month+1;
-        }
-        String final_date="the next day is: "+ new_day+", "+new_month+", "+new_year;
+        String final_date = "the next day is: " + new_day + ", " + new_month + ", " + new_year;
         System.out.println(final_date);
-        return(final_date);
+        return new date(new_day, new_month, new_year);
+    }
+
+
+    //days of year meathod pseudocode-using the month loop through using switch statemtnets and then add the number of days on top
+    public static int daysOfYear(int day, int month) {
+        int days_passed = 0;
+
+        for (int i = 0; i < month; i++) {
+
+            days_passed = days_passed + daysInMonth(i);
+
+        }
+        days_passed = days_passed + day;
+
+        days_passed = days_passed - 1;//accoutns for the first day
+        System.out.println(days_passed);
+        return days_passed;
+    }
+
+
+    public static boolean comesBefore(int day1, int month1, int year1, int day2, int month2, int year2) {
+        if (year1 == year2) {
+            if (month1 == month2) {
+                if (day1 < day2 || day1 == day2) {
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+            else if (month1 < month2) {
+                    return true;
+            }
+            else{
+                return false;
+            }
+        }
+        else if (year1 < year2) {
+                return true;
+
+        } else {
+            return false;
+        }
+
+
     }
 
 
 
-
-
-    date date1 = new date(22, 1,2008);
-    date date2 = new date(2,12,2001);
-    date date3= new date(19, 12,21);
 }
--- Module/src/test/java/MainTest.java
+++ Module/src/test/java/MainTest.java
@@ -4,11 +4,55 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
-    @Test
+   @Test
     void daysInMonth() {
         assertEquals(31, Main.daysInMonth(1));
         assertEquals(31, Main.daysInMonth(2));
-        assertEquals(30, Main.daysInMonth(9));
+        assertEquals(30, Main.daysInMonth(9));}
+    @Test
+    void tommorow(){
+       //random date
+       assertEquals(
+               new Main.date(10,7,2026),
+               Main.tommorow(9,7,2026)
+       );
+       //end of month
+        assertEquals(
+                new Main.date(1,2,2026),
+                Main.tommorow(31,1,2026)
+        );
+        //end of year
+        assertEquals(
+                new Main.date(1,1,2027),
+                Main.tommorow(31,12,2026)
+        );
+
+
+
+
     }
+
+    @Test
+    void daysOfYear(){
+       assertEquals(159,Main.daysOfYear(9,6));
+       assertEquals(0,Main.daysOfYear(1,1));
+        assertEquals(1,Main.daysOfYear(2,1));
+    }
+
+    @Test
+    void comesBefore(){
+        assertEquals(true, Main.comesBefore(1, 1, 2025, 1, 1, 2025));
+        assertEquals(true, Main.comesBefore(20, 12, 2024, 1, 1, 2025));
+        assertEquals(false, Main.comesBefore(1, 1, 2026, 31, 12, 2025));
+    }
+    @Test
+    void invalidDates() {
+
+
+        assertThrows(IllegalArgumentException.class, () -> {new Main.date(32, 1, 2025);});
+
+        assertThrows(IllegalArgumentException.class, () -> {new Main.date(0, 1, 2025);});
+    }
+
 
 }
